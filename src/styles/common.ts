430e90200959beada640a9169861df4d1138e585
daseul <7581058@gmail.com>
2024-03-19 17:35:09 +0900

    💄Design: 공통 색상, 폰트사이즈 정의

---
+++ src/styles/common.ts
@@ -0,0 +1,17 @@
+export const Common = {
+  colors: {
+    primary: '#87C5FB',
+    secondary: '#B2D9FB',
+    point: '#FBF9C2',
+    white: '#F8F8F8',
+    black: '#222222',
+    gray: '#EDEDED',
+  },
+  fontSize: {
+    title: '2.4rem',
+    fs20: '2rem',
+    fs18: '1.8rem',
+    fs16: '1.6rem',
+    fs14: '1.4rem',
+  },
+}
