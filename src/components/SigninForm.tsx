e1bcf6f4dc749ca0ae2e6440e62e2cc4551cd810
daseul <7581058@gmail.com>
2024-03-27 20:14:24 +0900

    ✨ Feat: signin form 컴포넌트 구현

--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -1,18 +1,47 @@
 import { css, Theme } from '@emotion/react'
+import { useForm } from 'react-hook-form'
+import ErrorMessage from './ErrorMessage'
 
+import { login } from '@/api'
 import { Common } from '@/styles/common'
+import { signinFormValidation } from '@/utils/validation'
+interface SignInFormType {
+  email: string
+  password: string
+}
 
 const SigninForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormType>()
+
+  const onSubmit = async (data: SignInFormType) => {
+    alert(JSON.stringify(data))
+    try {
+      const res = await login({ email: data.email, password: data.password })
+      if (res) {
+        alert('로그인성공')
+      } else {
+        alert('로그인실패')
+      }
+    } catch (error) {
+      alert(`로그인실패${error}`)
+    }
+  }
+
   return (
-    <form css={formWrap}>
+    <form css={formWrap} onSubmit={handleSubmit(onSubmit)}>
       <label css={inputLabel} htmlFor="email">
         E-mail
       </label>
-      <input type="text" id="email" css={input} />
+      <input type="text" id="email" css={input} {...register('email', signinFormValidation.email)} />
       <label css={inputLabel} htmlFor="password">
         Password
       </label>
-      <input type="password" id="password" css={input} />
+      <input type="password" id="password" css={input} {...register('password', signinFormValidation.password)} />
+      <ErrorMessage msg={errors.email?.message || errors.password?.message} />
       <button type="submit" css={signinButton}>
         SignIn
       </button>
@@ -53,7 +82,7 @@ const signinButton = (theme: Theme) => css`
   height: 3rem;
   background-color: ${theme.button};
   color: ${theme.buttonText};
-  margin-top: 5rem;
+  margin-top: 2rem;
   font-size: ${Common.fontSize.fs12};
   cursor: pointer;
   &:hover {
