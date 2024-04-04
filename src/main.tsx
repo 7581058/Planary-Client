b9461ca9df2460bb2e14f7d8170929ee0993b29d
daseul <7581058@gmail.com>
2024-04-04 16:40:00 +0900

    🐛 Fix: msw 요청 경합 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { worker } from "./mocks/browser"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 
 import { routues } from '@/pages/Router.tsx'
 
-if (process.env.NODE_ENV === "development") {
-  worker.start()
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+  const { worker } = await import('./mocks/browser')
+  return worker.start()
 }
 
 const router = createBrowserRouter(routues)
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
+    </React.StrictMode>,
+  )
+})
