c1dd68db14ac7d2fb0bb112ddd10414a79d15e80
daseul <7581058@gmail.com>
2024-04-11 21:09:17 +0900

    💄 Style: mainnav 변경된 디자인, stylelint 적용

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -17,7 +17,6 @@ const MainNav = () => {
         {NAV_MENU.map((item, index: number) => (
           <NavLink key={index} css={menuItem} to={item.path} className={({ isActive }) => (isActive ? 'active' : '')}>
             <div css={menuIcon}>{item.icon}</div>
-            {item.title}
           </NavLink>
         ))}
       </div>
@@ -28,32 +27,36 @@ const MainNav = () => {
 export default MainNav
 
 const navContainer = (theme: Theme) => css`
-  height: 100%;
-  background-color: ${rgba(theme.background, 0.5)};
   display: flex;
   flex-shrink: 0;
+  height: 100%;
+  background-color: ${rgba(theme.background, 0.5)};
 `
 
 const menuWrap = css`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 40px;
   margin-top: 50px;
 `
 
 const menuItem = (theme: Theme) => css`
-  width: 100%;
-  height: 32px;
-  padding: 0 30px 0 30px;
+  cursor: pointer;
+
   display: flex;
   align-items: center;
-  gap: 15px;
-  font-size: ${Common.fontSize.fs12};
-  color: ${theme.darkGray};
-  cursor: pointer;
+
+  width: 100%;
+  height: 32px;
+  padding: 0 30px;
+
+  font-size: ${Common.fontSize.fs16};
+  color: ${theme.text};
+
   &:hover {
     scale: 1.05;
   }
+
   &.active {
     color: ${theme.button};
   }
@@ -61,8 +64,8 @@ const menuItem = (theme: Theme) => css`
 
 const menuIcon = css`
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
 `
 
 const NAV_MENU = [
