db712d937be43346b797c05814ba027a7efd3621
daseul <7581058@gmail.com>
2024-04-16 02:32:45 +0900

    💄 Style: alert 스타일링

--- src/components/alert/CustomAlert.tsx
+++ src/components/alert/CustomAlert.tsx
@@ -1,8 +1,9 @@
 import { css, keyframes, useTheme } from '@emotion/react'
 import { Theme } from '@emotion/react'
 
+import { AlertButtonType, AlertNotificationType } from '@/constants/enum'
 import { useAlert } from '@/hooks/useAlert'
-import { AlertButtonType } from '@/store/alertState'
+import { Common } from '@/styles/common'
 
 const CustomAlert = () => {
   const theme = useTheme()
@@ -10,11 +11,25 @@ const CustomAlert = () => {
 
   return (
     <div css={alertContainer(theme, alertState.isOpen)}>
-      <div>{alertState.title}</div>
-      <div>{alertState.content}</div>
+      {alertState.icon && <div>{alertState.icon}</div>}
+      <div css={titleWrap}>{alertState.title}</div>
+      <div css={contentWrap}>{alertState.content}</div>
       <div css={buttonWrap}>
-        {alertState.type === AlertButtonType.CloseAndOk && <button onClick={alertState.callBack}>Ok</button>}
-        {alertState.type === AlertButtonType.Close && <button onClick={closeAlert}>Close</button>}
+        {alertState.buttonType === AlertButtonType.CloseAndOk && (
+          <>
+            <button onClick={closeAlert} css={alertButton}>
+              취소
+            </button>
+            <button onClick={alertState.callBack} css={[alertButton, okButton]}>
+              확인
+            </button>
+          </>
+        )}
+        {alertState.buttonType === AlertButtonType.Close && (
+          <button onClick={closeAlert} css={[alertButton, alertColor(theme, alertState.notiType)]}>
+            닫기
+          </button>
+        )}
       </div>
     </div>
   )
@@ -29,30 +44,84 @@ const alertContainer = (theme: Theme, isOpen: boolean) => css`
 
   display: flex;
   flex-direction: column;
+  gap: 10px;
   align-items: center;
   justify-content: center;
 
+  box-sizing: border-box;
   height: fit-content;
   margin: 0 auto;
-  padding: 30px;
+  padding: 10px 30px;
 
   visibility: ${isOpen ? 'visible' : 'hidden'};
   background-color: ${theme.alertBackground};
+  border: 1px solid ${theme.border};
+  border-radius: 16px;
 
   transition: visibility 0.3s ease-out;
   animation: ${isOpen ? fadeIn : fadeOut} 0.3s;
 `
 
+const alertColor = (theme: Theme, notiType: AlertNotificationType) => {
+  let borderColor
+
+  switch (notiType) {
+    case AlertNotificationType.Error:
+      borderColor = theme.errorRed
+      break
+    case AlertNotificationType.Success:
+      borderColor = theme.successBlue
+      break
+    case AlertNotificationType.Info:
+      borderColor = theme.infoGreen
+      break
+    case AlertNotificationType.Warning:
+      borderColor = theme.warningYellow
+      break
+    default:
+      return null
+  }
+  return css`
+    color: ${borderColor};
+    border: 1px solid ${borderColor};
+  `
+}
+
+const alertButton = (theme: Theme) => css`
+  cursor: pointer;
+
+  padding: 5px 20px;
+
+  background-color: ${theme.alertButtonBackground};
+  border: 1px solid ${theme.alertButtonBoard};
+  border-radius: 4px;
+`
+const okButton = (theme: Theme) => css`
+  color: ${theme.alertOkButtonText};
+  background-color: ${theme.alertOkButton};
+`
+
 const fadeOut = keyframes`
-  from {top: 30px; opacity: 1;} 
-  to {top: 0; opacity: 0;}
+  from { top: 30px; opacity: 1; } 
+  to { top: 0; opacity: 0; }
 `
 
 const fadeIn = keyframes`
-  from {top: 0; opacity: 0;}
-  to {top: 30px; opacity: 1;}
+  from { top: 0; opacity: 0; }
+  to { top: 30px; opacity: 1; }
 `
 
 const buttonWrap = css`
   display: flex;
+  gap: 10px;
+`
+
+const titleWrap = css`
+  font-size: ${Common.fontSize.fs10};
+  font-weight: 700;
+`
+
+const contentWrap = css`
+  text-align: center;
+  white-space: pre-wrap;
 `
--- src/constants/alert.ts
+++ src/constants/alert.ts
@@ -4,6 +4,6 @@ export const LOGIN_FAILED_ALERT = {
   icon: '🔒',
   title: '로그인 실패',
   content: '이메일 또는 비밀번호가 잘못 입력 되었습니다. \n이메일과 비밀번호를 정확히 입력해 주세요.',
-  buttonType: AlertButtonType.None,
+  buttonType: AlertButtonType.Close,
   notiType: AlertNotificationType.Error,
 }
