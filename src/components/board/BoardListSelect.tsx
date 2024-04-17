7578431dacfb6f3b1acb39ec3099836fc392481d
daseul <7581058@gmail.com>
2024-04-18 01:05:27 +0900

    ✨ Feat: board list select 구현

---
+++ src/components/board/BoardListSelect.tsx
@@ -0,0 +1,36 @@
+import { css } from '@emotion/react'
+import { Theme } from '@emotion/react'
+import React from 'react'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+
+import { currentBoardId, currentBoardListQuery } from '@/store/boardState'
+
+interface Option {
+  index: number
+  title: string
+}
+
+const BoardListSelect = () => {
+  const setId = useSetRecoilState(currentBoardId)
+  const { boards } = useRecoilValue(currentBoardListQuery)
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    setId(Number(value))
+  }
+
+  return (
+    <select onChange={handleChangeValue} css={selectContainer}>
+      {boards &&
+        boards.map((option: Option, index: number) => (
+          <option key={index} value={option.index}>
+            {option.title}
+          </option>
+        ))}
+    </select>
+  )
+}
+
+export default BoardListSelect
+
+const selectContainer = (theme: Theme) => css``
