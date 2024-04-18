97a480eea917b7c5ca99f56b83a76c8a82a3a818
daseul <7581058@gmail.com>
2024-04-19 00:50:17 +0900

    ✨ Feat: board move 분리

--- src/components/board/BoardGrid.tsx
+++ src/components/board/BoardGrid.tsx
@@ -1,15 +1,9 @@
 import { css } from '@emotion/react'
-import update from 'immutability-helper'
-import { useCallback, useState } from 'react'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import Clock from '../widget/Clock'
+import { useRecoilValue } from 'recoil'
 
 import Panel from '@/components/board/Panel'
-
+import { currentBoardQuery } from '@/store/boardState'
 export interface Item {
-  id: number
-  title: string
   row: number
   col: number
   component: JSX.Element
@@ -20,91 +14,15 @@ export interface ContainerState {
 }
 
 const BoardGrid = () => {
-  const [panels, setPanels] = useState([
-    {
-      id: 0,
-      title: 'profile',
-      row: 1,
-      col: 3,
-      component: <div>profile</div>,
-    },
-    {
-      id: 1,
-      title: 'player',
-      row: 1,
-      col: 2,
-      component: <div>player</div>,
-    },
-    {
-      id: 2,
-      title: 'dday',
-      row: 1,
-      col: 2,
-      component: <div>dday</div>,
-    },
-    {
-      id: 3,
-      title: 'minicalendar',
-      row: 2,
-      col: 3,
-      component: <div>minicalendar</div>,
-    },
-    {
-      id: 4,
-      title: 'clock',
-      row: 1,
-      col: 2,
-      component: <Clock />,
-    },
-    {
-      id: 5,
-      title: 'timer',
-      row: 2,
-      col: 2,
-      component: <div>timer</div>,
-    },
-    {
-      id: 6,
-      title: 'todo',
-      row: 1,
-      col: 2,
-      component: <div>todo</div>,
-    },
-  ])
-
-  const movePanel = useCallback((dragIndex: number, hoverIndex: number) => {
-    setPanels((prevPanels: Item[]) =>
-      update(prevPanels, {
-        $splice: [
-          [dragIndex, 1],
-          [hoverIndex, 0, prevPanels[dragIndex] as Item],
-        ],
-      }),
-    )
-  }, [])
-
-  const renderPanel = useCallback(
-    (panel: { id: number; title: string; row: number; col: number; component: JSX.Element }, index: number) => {
-      return (
-        <Panel
-          key={panel.id}
-          index={index}
-          id={panel.id}
-          title={panel.title}
-          row={panel.row}
-          col={panel.col}
-          movePanel={movePanel}
-          component={panel.component}
-        />
-      )
-    },
-    [movePanel],
-  )
+  const { boards } = useRecoilValue(currentBoardQuery)
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div css={boardWrap}>{panels.map((item, index) => renderPanel(item, index))}</div>
-    </DndProvider>
+    <div css={boardWrap}>
+      {boards &&
+        boards.grid.map((item: Item, index: number) => (
+          <Panel key={index} row={item.row} col={item.col} component={item.component}></Panel>
+        ))}
+    </div>
   )
 }
 
@@ -113,11 +31,12 @@ export default BoardGrid
 const boardWrap = css`
   overflow: hidden;
   display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(6, 1fr);
   gap: 15px;
 
   box-sizing: border-box;
   width: 100%;
   height: 100%;
+  padding: 0 40px 40px;
 `
--- src/components/board/Panel.tsx
+++ src/components/board/Panel.tsx
@@ -1,72 +1,19 @@
 import { css, useTheme } from '@emotion/react'
 import { Theme } from '@emotion/react'
-import type { Identifier } from 'dnd-core'
-import { useRef } from 'react'
-import { useDrag, useDrop } from 'react-dnd'
 
 export interface PanelProps {
-  id: number
-  title: string
-  index: number
   row: number
   col: number
-  movePanel: (dragIndex: number, hoverIndex: number) => void
   component: JSX.Element
 }
 
-interface DragItem {
-  index: number
-  id: string
-  type: string
-}
-
-const Panel = ({ id, title, index, row, col, movePanel, component }: PanelProps) => {
+const Panel = ({ row, col, component }: PanelProps) => {
   const theme = useTheme()
-  const ref = useRef<HTMLDivElement>(null)
 
-  //드래그
-  const [{ isDragging }, drag] = useDrag({
-    type: 'panel',
-    item: () => {
-      return { id, index }
-    },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  })
-  //드롭
-  const [{ handlerId }, drop] = useDrop<DragItem, void, { handlerId: Identifier | null }>({
-    accept: 'panel',
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      }
-    },
-
-    hover(item: DragItem) {
-      if (!ref.current) {
-        return
-      }
-      const dragIndex = item.index
-      const hoverIndex = index
-
-      if (dragIndex === hoverIndex) {
-        return
-      }
-
-      movePanel(dragIndex, hoverIndex)
-
-      item.index = hoverIndex
-    },
-  })
-
-  drag(drop(ref))
   return (
-    <div ref={ref} css={panelContainer(isDragging, row, col, theme)}>
+    <div css={panelContainer(row, col, theme)}>
       <div css={panelWrap}>
-        <div css={widget} data-handler-id={handlerId}>
-          {component}
-        </div>
+        <div css={widget}>{component}</div>
       </div>
     </div>
   )
@@ -74,7 +21,7 @@ const Panel = ({ id, title, index, row, col, movePanel, component }: PanelProps)
 
 export default Panel
 
-const panelContainer = (isDragging: boolean, row: number, col: number, theme: Theme) => css`
+const panelContainer = (row: number, col: number, theme: Theme) => css`
   position: relative;
 
   grid-column: span ${col};
@@ -82,7 +29,6 @@ const panelContainer = (isDragging: boolean, row: number, col: number, theme: Th
 
   box-sizing: border-box;
 
-  opacity: ${isDragging ? 0 : 1};
   background-color: ${theme.panel};
   border: 5px solid ${theme.border};
   border-radius: 16px;
