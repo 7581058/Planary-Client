720bbdf415e3d9751556856df27263d0920b75e9
daseul <7581058@gmail.com>
2024-04-25 14:41:47 +0900

    ✨ Feat: board edit mock data 정의

---
+++ src/mocks/data.ts
@@ -0,0 +1,13 @@
+// 대시보드
+///TODO: 반응형작업 md 등 추가 필요
+export const layoutData = {
+  lg: [
+    { i: '0', x: 0, y: 0, w: 3, h: 1, minW: 2, maxW: 4, minH: 1, maxH: 2, component: 'profile' },
+    { i: '1', x: 3, y: 0, w: 2, h: 1, minW: 1, maxW: 2, minH: 1, maxH: 2, component: 'clock' },
+    { i: '2', x: 5, y: 0, w: 2, h: 1, minW: 1, maxW: 4, minH: 1, maxH: 2, component: 'player' },
+    { i: '3', x: 0, y: 1, w: 3, h: 3, minW: 1, maxW: 4, minH: 1, maxH: 3, component: 'minicalendar' },
+    { i: '4', x: 3, y: 1, w: 2, h: 3, minW: 1, maxW: 3, minH: 1, maxH: 3, component: 'todo' },
+    { i: '5', x: 5, y: 1, w: 2, h: 1, minW: 1, maxW: 2, minH: 1, maxH: 2, component: 'dday' },
+    { i: '6', x: 5, y: 2, w: 2, h: 2, minW: 1, maxW: 2, minH: 1, maxH: 2, component: 'timer' },
+  ],
+}
