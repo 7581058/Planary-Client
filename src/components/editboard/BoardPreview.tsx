c62ef31cd724f3460d232a9bd025bf3cc17137a0
daseul <7581058@gmail.com>
2024-05-03 04:32:11 +0900

    ✨ Feat: boardpreview 편집 dirty flag 적용

--- src/components/editboard/BoardPreview.tsx
+++ src/components/editboard/BoardPreview.tsx
@@ -1,29 +1,18 @@
 import { Theme } from '@emotion/react'
 import { css } from '@emotion/react'
 import { useEffect } from 'react'
-import { Responsive, WidthProvider } from 'react-grid-layout'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { Layout, Layouts, Responsive, WidthProvider } from 'react-grid-layout'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import Panel from '../board/Panel'
 
-import { BoardItem, BoardState, boardState, currentBoardQuery } from '@/store/boardState'
-
-interface Layouts {
-  [P: string]: Layout[]
-}
-
-export interface Layout {
-  i: string
-  x: number
-  y: number
-  w: number
-  h: number
-}
+import { boardDirtyFlag, BoardItem, BoardState, boardState, currentBoardQuery } from '@/store/boardState'
 
 const ResponsiveGridLayout = WidthProvider(Responsive)
 
 const BoardPreview = () => {
   const [boards, setBoards] = useRecoilState<BoardState>(boardState)
   const boardData = useRecoilValue(currentBoardQuery)
+  const setIsDirty = useSetRecoilState<boolean>(boardDirtyFlag)
 
   useEffect(() => {
     if (boardData) {
@@ -41,31 +30,67 @@ const BoardPreview = () => {
     })
   }
 
-  const onLayoutChange = (newLayout: BoardItem[]) => {
+  const onLayoutChange = (currentLayout: Layout[] | BoardState) => {
+    const prevLayoutString = JSON.stringify(convertBoardStateToLayouts(boards).lg)
+    const newLayoutString = JSON.stringify(convertBoardStateToLayouts(currentLayout).lg)
+
+    if (prevLayoutString !== newLayoutString) {
+      setIsDirty(true)
+    }
+
     setBoards((prevBoards) => {
-      const updatedLayouts = newLayout.map((newItem: BoardItem) => {
-        const foundItem: BoardItem | undefined = prevBoards.lg.find((prevItem: BoardItem) => prevItem.i === newItem.i)
-        if (foundItem) {
-          return {
-            ...newItem,
-            component: foundItem.component,
+      if ('lg' in currentLayout && Array.isArray(currentLayout.lg)) {
+        const updatedLayouts = currentLayout.lg.map((newItem) => {
+          const foundItem: BoardItem | undefined = prevBoards.lg.find((prevItem: BoardItem) => prevItem.i === newItem.i)
+          if (foundItem) {
+            return {
+              ...newItem,
+              component: foundItem.component,
+            }
           }
-        }
-        return newItem
-      })
-      return { ...prevBoards, lg: updatedLayouts }
+          return newItem
+        })
+        return { ...prevBoards, lg: updatedLayouts }
+      } else {
+        return prevBoards
+      }
     })
   }
 
-  const convertBoardStateToLayouts = (boardState: BoardState): Layouts => {
-    const origin = boardState.lg.map((item) => ({
-      i: item.i,
-      x: item.x,
-      y: item.y,
-      w: item.w,
-      h: item.h,
-    }))
-    return { lg: origin }
+  const convertBoardStateToLayouts = (boardState: BoardState | Layout[]): Layouts => {
+    const layouts: Layouts = {}
+
+    if (!boardState) {
+      return layouts
+    }
+
+    if ('lg' in boardState && Array.isArray(boardState.lg)) {
+      layouts.lg = boardState.lg.map((item) => ({
+        i: item.i,
+        x: item.x,
+        y: item.y,
+        w: item.w,
+        h: item.h,
+        minW: item.minW,
+        maxW: item.maxW,
+        minH: item.minH,
+        maxH: item.maxH,
+      }))
+    } else if (Array.isArray(boardState)) {
+      layouts.lg = boardState.map((item) => ({
+        i: item.i,
+        x: item.x,
+        y: item.y,
+        w: item.w,
+        h: item.h,
+        minW: item.minW,
+        maxW: item.maxW,
+        minH: item.minH,
+        maxH: item.maxH,
+      }))
+    }
+
+    return layouts
   }
 
   return (
