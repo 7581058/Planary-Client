373106eb1fd096eb67e01de752514e41abc85073
daseul <7581058@gmail.com>
2024-05-03 04:44:47 +0900

    ✨ Feat: board edit 페이지 구성

--- src/pages/BoardEdit.tsx
+++ src/pages/BoardEdit.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { css } from '@emotion/react'
+import React, { useEffect } from 'react'
+import { useRecoilValue } from 'recoil'
+
+import BoardEditMenu from '@/components/editboard/BoardEditMenu'
+import BoardPreview from '@/components/editboard/BoardPreview'
+import WidgetList from '@/components/editboard/WidgetList'
+import PageHeader from '@/components/header/PageHeader'
+import { boardDirtyFlag } from '@/store/boardState'
 
 const BoardEdit = () => {
-  return <div>BoardEdit</div>
+  const isDirty = useRecoilValue(boardDirtyFlag)
+
+  useEffect(() => {
+    const handleWindowClose = (e: BeforeUnloadEvent) => {
+      if (!isDirty) return
+
+      e.preventDefault()
+    }
+
+    window.addEventListener('beforeunload', handleWindowClose)
+
+    return () => {
+      window.removeEventListener('beforeunload', handleWindowClose)
+    }
+  }, [isDirty])
+
+  return (
+    <div css={pageContainer}>
+      <PageHeader title={'Dashboard Edit'} />
+      <React.Suspense fallback={<></>}>
+        <div css={innerContainer}>
+          <WidgetList />
+          <BoardPreview />
+          <BoardEditMenu />
+        </div>
+      </React.Suspense>
+    </div>
+  )
 }
 
 export default BoardEdit
+
+const pageContainer = css`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const innerContainer = css`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`
