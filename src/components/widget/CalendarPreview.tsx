c41c758cd8f2ca92fd8a9a473253640a3e548778
daseul <7581058@gmail.com>
2024-05-19 00:51:20 +0900

    ✨ Feat: calendar preview 구성

--- src/components/widget/CalendarPreview.tsx
+++ src/components/widget/CalendarPreview.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import { Theme } from '@emotion/react'
+import { css } from '@emotion/react'
+import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosArrowForward } from 'react-icons/io'
+
+import { rgba } from '@/utils/convertRGBA'
 
 const CalendarPreview = () => {
-  return <div>CalendarPreview</div>
+  return (
+    <div css={container}>
+      <div css={topWrap}>
+        <IoIosArrowBack />
+        <span>월</span>
+        <IoIosArrowForward />
+      </div>
+      <div css={bottomWrap}>
+        {Array.from({ length: 42 }, (_, index) => (
+          <div css={gridItem} key={index}></div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default CalendarPreview
+
+const container = css`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+  padding: 4px;
+`
+
+const topWrap = (theme: Theme) => css`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 30px;
+
+  color: ${theme.previewText};
+`
+
+const bottomWrap = css`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  gap: 5px;
+
+  width: 100%;
+  height: 100%;
+`
+
+const gridItem = (theme: Theme) => css`
+  background-color: ${rgba(theme.previewSubText, 0.3)};
+`
