aa14bb16b2b3d469882e71c6a551b11b3720b8fd
daseul <7581058@gmail.com>
2024-05-22 01:36:03 +0900

    💄 Style: profile preview 반응형 적용

--- src/components/widget/ProfilePreview.tsx
+++ src/components/widget/ProfilePreview.tsx
@@ -2,27 +2,29 @@ import { Theme } from '@emotion/react'
 import { css } from '@emotion/react'
 import { LiaCoinsSolid } from 'react-icons/lia'
 
+import { WidgetProps } from '@/constants/widget'
 import { Common } from '@/styles/common'
-const ProfilePreview = () => {
+
+const ProfilePreview = ({ w, h }: WidgetProps) => {
   return (
-    <div css={container}>
+    <div css={[container, responsiveContainer(w, h)]}>
       <div css={leftWrap}>
-        <div css={img}></div>
+        <div css={[img, responsiveImg(w, h)]}></div>
       </div>
-      <div css={rightWrap}>
-        <div css={titleWrap}>
-          <div css={userTitle}>타이틀1</div>
-          <div css={userTitle}>타이틀2</div>
+      <div css={[rightWrap, responsiveRight(w, h)]}>
+        <div css={[titleWrap, responsiveTitle(w, h)]}>
+          <div css={userTitle}>Title</div>
+          <div css={userTitle}>Title</div>
         </div>
-        <span css={userName}>Name</span>
-        <div css={levelWrap}>
+        <span css={[userName, responsiveName(w, h)]}>Name</span>
+        <div css={[levelWrap, responsiveLevelWrap(w, h)]}>
           <div css={levelBackground}></div>
-          <div css={levelBar}>1 level</div>
+          <div css={[levelBar, responsiveLevelBar(w, h)]}>1 level</div>
         </div>
         <div css={infoWrap}>
-          <div css={wallet}>
+          <div css={[coin, responsiveCoin(w, h)]}>
             <LiaCoinsSolid />
-            보유 코인: 0 코인
+            My Coin: 0 Coin
           </div>
         </div>
       </div>
@@ -34,14 +36,24 @@ export default ProfilePreview
 
 const container = css`
   display: flex;
+  gap: 4px;
+
   width: 100%;
   height: 100%;
   padding: 4px;
 `
 
+const responsiveContainer = (w: number, h: number) => css`
+  ${w === 2 && h === 2 && `flex-direction: column;`}
+`
+
 const leftWrap = css`
+  display: flex;
   flex-basis: 0;
   flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+
   width: 100%;
   height: 100%;
 `
@@ -53,16 +65,53 @@ const img = (theme: Theme) => css`
   border-radius: 50%;
 `
 
+const responsiveImg = (w: number, h: number) => css`
+  ${w === 0 &&
+  h === 0 &&
+  `width: 50px;
+  height: 50px;`}
+  ${w === 2 &&
+  h === 1 &&
+  `width: 80px;
+  height: 80px;`}
+  ${w === 4 &&
+  h === 1 &&
+  `border-radius: 0;
+  width: 100%;
+  height: 100%;`}
+  ${w === 2 &&
+  h === 2 &&
+  `border-radius: 0;
+  width: 100%;
+  height: 100%;`}
+  ${w === 3 &&
+  h === 2 &&
+  `border-radius: 0;
+  width: 100%;
+  height: 100%;`}
+  ${w === 4 &&
+  h === 2 &&
+  `border-radius: 0;
+  width: 100%;
+  height: 100%;`}
+`
+
 const rightWrap = css`
   display: flex;
   flex-basis: 0;
   flex-direction: column;
   flex-grow: 2.5;
   gap: 4px;
+  justify-content: center;
 
   width: 100%;
   height: 100%;
-  padding: 5px;
+`
+
+const responsiveRight = (w: number, h: number) => css`
+  ${w === 2 && h === 2 && `flex-grow: 1;gap: 5px;`}
+  ${w === 3 && h === 2 && `flex-grow: 1;gap: 10px;`}
+  ${w === 4 && h === 2 && `flex-grow: 1.5;gap: 15px;`}
 `
 
 const titleWrap = css`
@@ -81,18 +130,49 @@ const userTitle = (theme: Theme) => css`
   border-radius: 4px;
 `
 
+const responsiveTitle = (w: number, h: number) => css`
+  ${w === 0 && h === 0 && `font-size: ${Common.fontSize.fs6};`}
+  ${w === 2 && h === 1 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 3 && h === 1 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 4 && h === 1 && `font-size: ${Common.fontSize.fs9};`}
+  ${w === 2 && h === 2 && `font-size: ${Common.fontSize.fs9};`}
+  ${w === 3 && h === 2 && `font-size: ${Common.fontSize.fs10};`}
+  ${w === 4 && h === 2 && `font-size: ${Common.fontSize.fs12};`}
+`
+
 const userName = (theme: Theme) => css`
   font-size: ${Common.fontSize.fs14};
   color: ${theme.previewText};
 `
 
+const responsiveName = (w: number, h: number) => css`
+  ${w === 0 && h === 0 && `font-size: ${Common.fontSize.fs10};`}
+  ${w === 2 && h === 1 && `font-size: ${Common.fontSize.fs14};`}
+  ${w === 3 && h === 1 && `font-size: ${Common.fontSize.fs14};`}
+  ${w === 4 && h === 1 && `font-size: ${Common.fontSize.fs14};`}
+  ${w === 2 && h === 2 && `font-size: ${Common.fontSize.fs14};`}
+  ${w === 3 && h === 2 && `font-size: ${Common.fontSize.fs16};`}
+  ${w === 4 && h === 2 && `font-size: ${Common.fontSize.fs18};`}
+`
+
 const levelWrap = css`
   position: relative;
+  height: 20px;
+`
+
+const responsiveLevelWrap = (w: number, h: number) => css`
+  ${w === 0 && h === 0 && `height: 5px;`}
+  ${w === 2 && h === 1 && `height: 14px;`}
+  ${w === 3 && h === 1 && `height: 20px;`}
+  ${w === 4 && h === 1 && `height: 20px;`}
+  ${w === 2 && h === 2 && `height: 20px;`}
+  ${w === 3 && h === 2 && `height: 20px;`}
+  ${w === 4 && h === 2 && `height: 20px;`}
 `
 
 const levelBackground = (theme: Theme) => css`
   width: 100%;
-  height: 20px;
+  height: 100%;
   background-color: ${theme.previewSubText};
   border-radius: 10px;
 `
@@ -106,7 +186,7 @@ const levelBar = (theme: Theme) => css`
 
   box-sizing: border-box;
   width: 30%;
-  height: 20px;
+  height: 100%;
   padding-left: 10px;
 
   font-size: ${Common.fontSize.fs8};
@@ -115,12 +195,21 @@ const levelBar = (theme: Theme) => css`
   background-color: ${theme.previewPointText};
   border-radius: 10px;
 `
+const responsiveLevelBar = (w: number, h: number) => css`
+  ${w === 0 && h === 0 && `font-size: 0;`}
+  ${w === 2 && h === 1 && `padding-left: 6px;font-size: ${Common.fontSize.fs7};`}
+  ${w === 3 && h === 1 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 4 && h === 1 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 2 && h === 2 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 3 && h === 2 && `padding-left: 8px;font-size: ${Common.fontSize.fs8};`}
+  ${w === 4 && h === 2 && `font-size: ${Common.fontSize.fs10};`}
+`
 
 const infoWrap = css`
   display: flex;
 `
 
-const wallet = (theme: Theme) => css`
+const coin = (theme: Theme) => css`
   display: flex;
   align-items: center;
 
@@ -131,4 +220,14 @@ const wallet = (theme: Theme) => css`
   color: ${theme.previewText};
 
   border-radius: 4px;
+`
+
+const responsiveCoin = (w: number, h: number) => css`
+  ${w === 0 && h === 0 && `font-size: ${Common.fontSize.fs7};`}
+  ${w === 2 && h === 1 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 3 && h === 1 && `font-size: ${Common.fontSize.fs8};`}
+  ${w === 4 && h === 1 && `font-size: ${Common.fontSize.fs9};`}
+  ${w === 2 && h === 2 && `font-size: ${Common.fontSize.fs9};`}
+  ${w === 3 && h === 2 && `font-size: ${Common.fontSize.fs10};`}
+  ${w === 4 && h === 2 && `font-size: ${Common.fontSize.fs12};`}
 `
