e5b6f7cbea787ed9b3615c5fc9c3534fcc7bef4f
daseul <7581058@gmail.com>
2024-05-28 01:17:14 +0900

    💄 Style: calendar preview 반응형 적용

--- src/components/widget/CalendarPreview.tsx
+++ src/components/widget/CalendarPreview.tsx
@@ -3,21 +3,54 @@ import { css } from '@emotion/react'
 import { IoIosArrowBack } from 'react-icons/io'
 import { IoIosArrowForward } from 'react-icons/io'
 
+import { WidgetProps } from '@/constants/widget'
 import { rgba } from '@/utils/convertRGBA'
 
-const CalendarPreview = () => {
+const CalendarPreview = ({ w, h }: WidgetProps) => {
+  const numSquares = 7 * 52
+
+  const isInvalidResizingSize = (type: string, w: number, h: number) => {
+    const monthlyInvalidSizes = [
+      { w: 1, h: 1 },
+      { w: 2, h: 2 },
+      { w: 3, h: 2 },
+      { w: 3, h: 3 },
+      { w: 4, h: 3 },
+    ]
+    const noneInvalidSizes = [
+      { w: 1, h: 3 },
+      { w: 1, h: 2 },
+      { w: 2, h: 3 },
+      { w: 4, h: 2 },
+    ]
+    const invalidSizes = type === 'monthly' ? monthlyInvalidSizes : noneInvalidSizes
+    return invalidSizes.some((size) => size.w === w && size.h === h)
+  }
+
   return (
     <div css={container}>
-      <div css={topWrap}>
-        <IoIosArrowBack />
-        <span>월</span>
-        <IoIosArrowForward />
-      </div>
-      <div css={bottomWrap}>
-        {Array.from({ length: 42 }, (_, index) => (
-          <div css={gridItem} key={index}></div>
-        ))}
-      </div>
+      {isInvalidResizingSize('monthly', w, h) ? (
+        <>
+          <div css={topWrap}>
+            <IoIosArrowBack />
+            <span>0월</span>
+            <IoIosArrowForward />
+          </div>
+          <div css={bottomWrap}>
+            {Array.from({ length: 42 }, (_, index) => (
+              <div css={gridItem} key={index}></div>
+            ))}
+          </div>
+        </>
+      ) : isInvalidResizingSize('none', w, h) ? (
+        <></>
+      ) : (
+        <div css={yearGrid}>
+          {Array.from({ length: numSquares }).map((_, index) => (
+            <div css={yearGridItem} key={index}></div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
@@ -57,4 +90,17 @@ const bottomWrap = css`
 
 const gridItem = (theme: Theme) => css`
   background-color: ${rgba(theme.previewSubText, 0.3)};
+`
+
+const yearGrid = () => css`
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20px, 1fr));
+  gap: 2px;
+`
+
+const yearGridItem = (theme: Theme) => css`
+  width: 14px;
+  height: 14px;
+  background-color: ${theme.previewSubText};
 `
