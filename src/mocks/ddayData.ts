be24c73df76ab49c3db5115e7f49a2a85fb91989
daseul <7581058@gmail.com>
2024-06-03 22:48:40 +0900

    ✨ Feat: dday mock data 추가

---
+++ src/mocks/ddayData.ts
@@ -0,0 +1,17 @@
+export const ddayData = [
+  {
+    index: 0,
+    title: '시험',
+    date: '20240912',
+  },
+  {
+    index: 1,
+    title: '축제',
+    date: '20240108',
+  },
+  {
+    index: 2,
+    title: '❤️',
+    date: '20191022',
+  },
+]
