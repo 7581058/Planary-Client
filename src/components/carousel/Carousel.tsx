a6becaf8b642beecbe4ae0fa6179a960e6026f98
daseul <7581058@gmail.com>
2024-06-04 00:29:03 +0900

    ✨ Feat: carousel 구현

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,162 @@
+import { Theme } from '@emotion/react'
+import { css, useTheme } from '@emotion/react'
+import React, { useEffect, useRef, useState } from 'react'
+
+interface CarouselProps {
+  items: React.ReactElement[]
+  auto?: boolean
+}
+
+const Carousel = ({ items, auto }: CarouselProps) => {
+  const theme = useTheme()
+  const [currentSlide, setCurrentSlide] = useState(1)
+  const [transition, setTransition] = useState(0.5)
+  const [slides, setSlides] = useState<React.ReactElement[]>([])
+
+  const timeoutRef = useRef<number | null>(null)
+
+  useEffect(() => {
+    if (items) {
+      const slideStart = items[0]
+      const slideEnd = items[items.length - 1]
+      const slideNew = [slideEnd, ...items, slideStart]
+      setSlides(slideNew)
+    }
+  }, [items])
+
+  useEffect(() => {
+    if (auto) {
+      timeoutRef.current = window.setTimeout(() => {
+        nextSlide()
+      }, 3000)
+      return () => {
+        if (timeoutRef.current !== null) {
+          clearTimeout(timeoutRef.current)
+        }
+      }
+    }
+  }, [currentSlide, auto])
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => prev + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => prev - 1)
+  }
+
+  const moveSlide = (index: number) => {
+    setTransition(0)
+    setCurrentSlide(index)
+    setTimeout(() => {
+      setTransition(0.5)
+    }, 50)
+  }
+
+  const handleTransitionEnd = () => {
+    if (currentSlide === slides.length - 1) {
+      moveSlide(1)
+    } else if (currentSlide === 0) {
+      moveSlide(slides.length - 2)
+    }
+  }
+
+  return (
+    <div css={container}>
+      <div css={carouselWrap(currentSlide, transition)} onTransitionEnd={handleTransitionEnd}>
+        {slides &&
+          slides.map((Component, index) => (
+            <div css={slide} key={index}>
+              {Component}
+            </div>
+          ))}
+      </div>
+      <button css={[moveButton, prevButton]} onClick={prevSlide}>
+        &lt;
+      </button>
+      <button css={[moveButton, nextButton]} onClick={nextSlide}>
+        &gt;
+      </button>
+
+      <div css={paginationWrap}>
+        {items.map((_, index) => (
+          <div
+            css={paginationDot(theme, index + 1 === currentSlide)}
+            key={index}
+            onClick={() => moveSlide(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Carousel
+
+const container = css`
+  position: relative;
+
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100%;
+`
+
+const paginationWrap = css`
+  display: flex;
+  flex-shrink: 0;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+`
+
+const carouselWrap = (currentSlide: number, transition: number) => css`
+  transform: translateX(-${currentSlide * 100}%);
+
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+
+  transition: transform ${transition}s ease-in-out;
+`
+
+const slide = css`
+  min-width: 100%;
+  min-height: 100%;
+`
+
+const moveButton = css`
+  cursor: pointer;
+
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+
+  font-size: 2rem;
+
+  background: none;
+  border: none;
+`
+
+const prevButton = css`
+  left: 0;
+`
+
+const nextButton = css`
+  right: 0;
+`
+
+const paginationDot = (theme: Theme, active: boolean) => css`
+  cursor: pointer;
+
+  width: 8px;
+  height: 8px;
+
+  background-color: ${active ? theme.previewText : theme.previewSubText};
+  border-radius: 50%;
+`
