6cc8b849a59f637d87262f6c2fd1076f5f3d31d4
daseul <7581058@gmail.com>
2024-06-13 02:14:23 +0900

    ✨ Feat: convertDate dot case 출력 형식 변경

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -10,7 +10,7 @@ export const convertDate = (date: string, type: string) => {
 
   switch (type) {
     case 'dot': {
-      return (formattedDate = `${year}.${month.padStart(2, '0')}.${day.padStart(2, '0')}.${dayNames[fullDate.getDay()].slice(0, 1)}`)
+      return (formattedDate = `${year}.${month.padStart(2, '0')}.${day.padStart(2, '0')}(${dayNames[fullDate.getDay()].slice(0, 1)})`)
     }
     case 'kor': {
       return (formattedDate = `${year}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일 ${dayNames[fullDate.getDay()]}`)
