08a6b3d654520ccc3bd5f0d7fb46f2badd23af5c
daseul <7581058@gmail.com>
2024-06-25 19:08:26 +0900

    ✨ Feat: page header paths 에 pageheader title 추가

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -6,3 +6,15 @@ export const PLANNER_PATH = '/planner'
 export const STORE_PATH = '/store'
 export const LOGIN_PATH = '/login'
 export const SIGNUP_PATH = '/signup'
+
+export const PAGE_HEADER_PATHS = [
+  {
+    path: DASHBOARD_EDIT_PATH,
+    title: '대시보드 편집',
+  },
+  {
+    path: SIGNUP_PATH,
+    title: '회원가입',
+  },
+]
+export const LAYOUT_HEADER_PATHS = [DASHBOARD_PATH]
