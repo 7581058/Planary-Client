edcacb62df0bef23bdd656e923872e64a1c549dd
daseul <7581058@gmail.com>
2024-06-26 22:58:14 +0900

    ✨ Feat: page header layout 에서 분리

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,30 +4,24 @@ import { Outlet, useLocation } from 'react-router-dom'
 
 import CustomAlert from '@/components/alert/CustomAlert'
 import LayoutHeader from '@/components/header/LayoutHeader'
-import PageHeader from '@/components/header/PageHeader'
 import MainNav from '@/components/MainNav'
 import ModalBase from '@/components/modal/ModalBase'
-import { LAYOUT_HEADER_PATHS, PAGE_HEADER_PATHS } from '@/constants/paths'
+import { LAYOUT_HEADER_PATHS } from '@/constants/paths'
 import { globalStyles } from '@/styles/globalStyles'
 import { themeDefault } from '@/styles/theme'
 
 const Layout = () => {
   const location = useLocation()
-  const isPageHeaderPath = PAGE_HEADER_PATHS.find((item) => item.path === location.pathname)
+  const modalEl = document.getElementById('modal')
+
   const isLayoutHeaderPath = LAYOUT_HEADER_PATHS.includes(location.pathname)
 
-  const modalEl = document.getElementById('modal')
   return (
     <ThemeProvider theme={themeDefault}>
       <Global styles={globalStyles} />
       <div css={container}>
-        {isPageHeaderPath ? (
-          <div css={innerContainer}>
-            <PageHeader title={isPageHeaderPath.title} />
-            <Outlet />
-          </div>
-        ) : isLayoutHeaderPath ? (
-          <div css={container}>
+        {isLayoutHeaderPath ? (
+          <div css={navContainer}>
             <MainNav />
             <div css={innerContainer}>
               <LayoutHeader />
@@ -62,6 +56,12 @@ const innerContainer = css`
   display: flex;
   flex-direction: column;
 
+  width: 100%;
+  height: 100%;
+`
+
+const navContainer = css`
+  display: flex;
   width: 100%;
   height: 100%;
 `
