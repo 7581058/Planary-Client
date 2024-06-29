b1dc8d3b8ecb6bc63a7734f9cdda4a0860452bdc
daseul <7581058@gmail.com>
2024-06-29 21:34:11 +0900

    ✨ Feat: signup 페이지 구현

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,67 @@
+import { css } from '@emotion/react'
+import { Theme } from '@emotion/react'
+
+import PageHeader from '@/components/header/PageHeader'
+import SignupForm from '@/components/SignupForm'
+import { Common } from '@/styles/common'
+
+const Signup = () => {
+  return (
+    <div css={container}>
+      <PageHeader title="회원가입" />
+      <div css={innerContainer}>
+        <div css={logoWrap}>
+          <img css={logo} src="/src/assets/logo_primary.svg" alt="planary" />
+          <p css={sub}>Discover the Power of an Organized Life. Join Us Today!</p>
+        </div>
+        <div css={formWrap}>
+          <SignupForm />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
+
+const container = css`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const formWrap = css`
+  width: 500px;
+  height: 80%;
+  padding: 16px;
+`
+
+const innerContainer = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+`
+
+const logo = css`
+  width: 120px;
+  height: 40px;
+`
+
+const sub = (theme: Theme) => css`
+  margin: 0;
+  font-size: ${Common.fontSize.fs8};
+  color: ${theme.subText};
+  text-align: center;
+`
+
+const logoWrap = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`
