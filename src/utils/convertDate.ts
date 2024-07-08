bc3e8025885266cccbe878136c58b30609415dbf
daseul <7581058@gmail.com>
2024-07-08 16:03:16 +0900

    ✨ Feat: 디데이 관련 utils 수정

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,22 +1,29 @@
-//원본 날짜 형식 yyyymmdd
 export const convertDate = (date: string, type: string) => {
   let formattedDate
-  const year = date.slice(0, 4)
-  const month = date.slice(4, 6)
-  const day = date.slice(6, 8)
+  const parsedDate = date.split('-')
+  const year = parsedDate[0]
+  const month = String(Number(parsedDate[1]))
+  const day = parsedDate[2]
 
-  const fullDate = new Date(`${year}-${month}-${day}`)
+  const fullDate = new Date(`${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`)
   const dayNames = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
 
   switch (type) {
     case 'dot': {
-      return (formattedDate = `${year}.${month.padStart(2, '0')}.${day.padStart(2, '0')}(${dayNames[fullDate.getDay()].slice(0, 1)})`)
+      formattedDate = `${year}.${month.padStart(2, '0')}.${day.padStart(2, '0')}(${dayNames[fullDate.getDay()].slice(0, 1)})`
+      break
     }
     case 'kor': {
-      return (formattedDate = `${year}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일 ${dayNames[fullDate.getDay()]}`)
+      formattedDate = `${year}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일 ${dayNames[fullDate.getDay()]}`
+      break
     }
     case 'dash': {
-      return (formattedDate = `${year}-${parseInt(month, 10)}-${parseInt(day, 10)} ${dayNames[fullDate.getDay()]}`)
+      formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')} ${dayNames[fullDate.getDay()]}`
+      break
+    }
+    default: {
+      formattedDate = ''
+      break
     }
   }
 
