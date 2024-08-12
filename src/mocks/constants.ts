1abf81c3e1d13901d12839fd8c597cd7c7dc0485
daseul <7581058@gmail.com>
2024-08-13 00:19:07 +0900

    ✨ Feat: msw 테스트 고정 계정 constants 로 분리

--- src/mocks/constants.ts
+++ src/mocks/constants.ts
@@ -6,3 +6,8 @@ export const MSW_DASHBOARD_LAYOUTS: { [key: number]: LayoutDataType[] } = {
   1: layoutData,
   2: layoutData2,
 }
+
+// 테스트 고정 계정
+export const MSW_TEST_ACCOUNTS = [
+  { username: 'test', email: 'test@planary.com', password: '1q2w3e4r#', birth: '19950505', agree: true },
+]
