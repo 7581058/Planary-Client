cbd7f25c09e6461e71b2fb40641d4421729eea28
daseul <7581058@gmail.com>
2024-08-13 00:20:51 +0900

    ✨ Feat: msw 토큰 검증 util 함수 추가

---
+++ src/mocks/utils.ts
@@ -0,0 +1,22 @@
+import { DefaultBodyType, HttpResponse, StrictRequest } from 'msw'
+import { MSW_TEST_TOKEN } from './constants'
+import { RES_TOKEN_FAIL_INVALID, RES_TOKEN_FAIL_MISSING } from './resMessage'
+
+//토큰 검증 (헤더에 있는지, 유효한 토큰인지)
+export const authenticateRequest = (request: StrictRequest<DefaultBodyType>) => {
+  const token = request.headers.get('Authorization')
+
+  if (!token) {
+    return HttpResponse.json(RES_TOKEN_FAIL_MISSING.res, {
+      status: RES_TOKEN_FAIL_MISSING.code,
+    })
+  }
+
+  if (token !== MSW_TEST_TOKEN) {
+    return HttpResponse.json(RES_TOKEN_FAIL_INVALID.res, {
+      status: RES_TOKEN_FAIL_INVALID.code,
+    })
+  }
+
+  return null // 인증 성공
+}
