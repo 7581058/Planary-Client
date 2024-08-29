18d0bcb29d19f54aa965214c1c25bebd4502ee77
daseul <7581058@gmail.com>
2024-08-29 23:44:07 +0900

    ⚙️ Chore: eslint extends 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true, jest: true, },
   extends: [
     "eslint:recommended",
+    "prettier/@typescript-eslint",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
